fbaaaea02da3a62544a4a7117ae1674e254f674c
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-01-18 13:51:55 -0700

    App wired to API and randomly generating jokes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import data from './data'
+import Card from './components/card'
+import chuck from './components/chuck-norris.png'
 
 class App extends Component {
-  render() {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      jokes: [],
+      wisdom: ''
+    }
+  }
+
+  getRandomIntInclusive = (max) => {
+    const min = 0
+    max = Math.floor(max)
+    return this.setState({wisdom: Math.floor(Math.random() * (max - min + 1)) + min}) //The maximum is inclusive and the minimum is inclusive 
+  }
+
+  generateQuote = () => {
+    this.getRandomIntInclusive(this.state.jokes.length - 1)
+  }
+
+  componentDidMount() {
+    fetch('https://chuck-norris-quote-generator.herokuapp.com/jokes')
+      .then(data => data.json())
+        .then(JSONdata => {
+          this.setState({jokes: JSONdata.data.jokes})
+        })
+  }
+
+  render() {    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row justify-content-center py-5">
+          <div className="col-8 text-center">
+          <img src={chuck} alt="chuck" style={{height: "200px"}}/>
+            <h1 className="pb-2">Hello Chuck!</h1>
+            <p className="pb-2">An app for randomly generating Chuck Norris jokes.</p>
+            <button className="btn btn-danger btn-lg" onClick={this.generateQuote}>Karate Chop</button>
+          </div>
+        </div> 
+        <div className="row justify-content-center">
+        {this.state.wisdom || this.state.wisdom === 0
+          ? <Card
+              // title={this.state.jokes[this.state.wisdom].title}
+              quote={this.state.jokes[this.state.wisdom].joke}
+              tags={this.state.jokes[this.state.wisdom].categories}/>
+          : <div></div>}
+        </div>
       </div>
     );
   }
